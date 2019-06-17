0c28edbe7c966e6cbd4dfec3796a12eeb62f0d6b
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-06-17 09:16:12 -0400

    Update and rename PhotosensitiveSens.py to 6-PhotosensitiveSens.py

--- InputsFinal/6-PhotosensitiveSens.py
+++ InputsFinal/6-PhotosensitiveSens.py
@@ -3,6 +3,7 @@ from gpiozero import LightSensor
 #Set variable of sensor to pin 18
 sensor = LightSensor(18)
 
+#Code below works properly but does not make sense?
 while True:
     sensor.wait_for_light()
     print("It's dark!")
