ca93fd462fe9a4470ebe299449991765aa3961df
Michael Hale <49446855+mjhale22@users.noreply.github.com>
2019-06-19 09:30:29 -0400

    Update 4-VibrationSens(SW-420).py

--- InputsFinal/4-VibrationSens(SW-420).py
+++ InputsFinal/4-VibrationSens(SW-420).py
@@ -11,9 +11,9 @@ GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
         if GPIO.input(channel):
-                print "Movement Detected!"
+                print ("Movement Detected!")
         else:
-                print "Movement Detected!"
+                print ("Movement Detected!")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
